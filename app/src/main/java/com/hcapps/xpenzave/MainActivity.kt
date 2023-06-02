62edbf17d2ab5e09ab3c6c478581317b7bb214ce
Hiten <hitenchawda24@gmail.com>
2023-06-02 16:39:42 +0530

    setup Material 3

--- app/src/main/java/com/hcapps/xpenzave/MainActivity.kt
+++ app/src/main/java/com/hcapps/xpenzave/MainActivity.kt
@@ -3,15 +3,7 @@ package com.hcapps.xpenzave
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.hcapps.xpenzave.ui.theme.XpenzaveTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,32 +11,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             XpenzaveTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface {
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    XpenzaveTheme {
-        Greeting("Android")
     }
 }
