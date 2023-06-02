8e39e640877d85e2974c5f4159b04a2b7be94666
Hiten <hitenchawda24@gmail.com>
2023-06-02 21:46:47 +0530

    Added loading in Register Button

--- app/src/main/java/com/hcapps/xpenzave/presentation/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/hcapps/xpenzave/presentation/auth/AuthenticationScreen.kt
@@ -11,20 +11,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -35,7 +40,9 @@ import com.hcapps.xpenzave.ui.theme.ButtonHeight
 
 @Composable
 fun AuthenticationScreen(navigateToHome: () -> Unit) {
+
     val context = LocalContext.current
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -141,20 +148,38 @@ fun RegisterBottomComponent(
     onClickOfGoogle: () -> Unit,
     onClickOfFaceBook: () -> Unit
 ) {
+
+    var loadingState by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(
-            onClick = { onClickOfRegisterButton() },
-            modifier = Modifier.fillMaxWidth().height(ButtonHeight),
+            onClick = {
+                loadingState = true
+                onClickOfRegisterButton()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(ButtonHeight),
             shape = Shapes().small,
+            enabled = !loadingState
         ) {
-            Text(
-                text = "Register",
-                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-                fontWeight = MaterialTheme.typography.labelLarge.fontWeight
-            )
+            if (!loadingState) {
+                Text(
+                    text = "Register",
+                    fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                    fontWeight = MaterialTheme.typography.labelLarge.fontWeight
+                )
+            }
+            if (loadingState) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(22.dp),
+                    strokeWidth = 2.dp,
+                    strokeCap = StrokeCap.Round
+                )
+            }
         }
 
         Spacer(modifier = Modifier.height(24.dp))
@@ -172,9 +197,12 @@ fun RegisterBottomComponent(
             FilledTonalButton(
                 onClick = { onClickOfFaceBook() },
                 shape = Shapes().small,
+                enabled = !loadingState,
                 colors = ButtonDefaults
                     .buttonColors(containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.1f)),
-                modifier = Modifier.height(ButtonHeight).weight(1f)
+                modifier = Modifier
+                    .height(ButtonHeight)
+                    .weight(1f)
             ) {
                 Text(
                     text = "FaceBook",
@@ -187,9 +215,12 @@ fun RegisterBottomComponent(
             FilledTonalButton(
                 onClick = { onClickOfGoogle() },
                 shape = Shapes().small,
+                enabled = !loadingState,
                 colors = ButtonDefaults
                     .buttonColors(containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.1f)),
-                modifier = Modifier.height(ButtonHeight).weight(1f)
+                modifier = Modifier
+                    .height(ButtonHeight)
+                    .weight(1f)
             ) {
                 Text(
                     text = "Google",
