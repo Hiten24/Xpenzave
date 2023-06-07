3bf1f9f85ce2415b756af861c7861379cdf1dd30
Hiten <hitenchawda24@gmail.com>
2023-06-07 14:17:48 +0530

    added Muli Fonts and FontFamily

---
+++ app/src/main/java/com/hcapps/xpenzave/ui/theme/Font.kt
@@ -0,0 +1,20 @@
+package com.hcapps.xpenzave.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.hcapps.xpenzave.R
+
+val MuliFontFamily = FontFamily(
+    listOf(
+        Font(R.font.muli),
+        Font(R.font.muli_bold),
+        Font(R.font.muli_bold_italic),
+        Font(R.font.muli_extra_light),
+        Font(R.font.muli_extra_light_italic),
+        Font(R.font.muli_italic),
+        Font(R.font.muli_light),
+        Font(R.font.muli_light_italic),
+        Font(R.font.muli_semi_bold),
+        Font(R.font.muli_semi_bold_italic),
+    )
+)
