62c6d07b0e97edae0cb39ae301107487e0c36407
Hiten <hitenchawda24@gmail.com>
2023-06-09 17:51:19 +0530

    created line graph

--- app/src/main/java/com/hcapps/xpenzave/presentation/general_stats/GeneralSection.kt
+++ app/src/main/java/com/hcapps/xpenzave/presentation/general_stats/GeneralSection.kt
@@ -2,18 +2,13 @@ package com.hcapps.xpenzave.presentation.general_stats
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.hcapps.xpenzave.presentation.compare.result.component.expense_category_graph.CategoryData
-import com.hcapps.xpenzave.presentation.compare.result.component.expense_category_graph.CategoryExpensesGraph
-import com.hcapps.xpenzave.presentation.home.component.BudgetProgressCard
 
 @Composable
 fun GeneralSection(modifier: Modifier = Modifier) {
@@ -26,7 +21,7 @@ fun GeneralSection(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.spacedBy(22.dp)
     ) {
 
-        BudgetProgressCard(
+        /*BudgetProgressCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight(0.45f),
@@ -36,7 +31,9 @@ fun GeneralSection(modifier: Modifier = Modifier) {
 
         CategoryExpensesGraph(
             categories = CategoryData.defaultCategoryGraphs()
-        )
+        )*/
+
+        DayExpenseGraph()
 
     }
 
