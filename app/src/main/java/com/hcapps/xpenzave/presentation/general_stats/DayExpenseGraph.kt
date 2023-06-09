ba42c124472a875923c9c2045ac43d808dbd0c7a
Hiten <hitenchawda24@gmail.com>
2023-06-09 18:05:56 +0530

    created zoom button in line graph

--- app/src/main/java/com/hcapps/xpenzave/presentation/general_stats/DayExpenseGraph.kt
+++ app/src/main/java/com/hcapps/xpenzave/presentation/general_stats/DayExpenseGraph.kt
@@ -6,10 +6,16 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Remove
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -18,7 +24,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -124,7 +129,9 @@ val data = listOf(
 )
 
 @Composable
-fun DayExpenseGraph() {
+fun DayExpenseGraph(
+    data: List<Pair<Int, Double>>
+) {
     Card(
         modifier = Modifier.fillMaxWidth()
     ) {
@@ -141,20 +148,46 @@ fun DayExpenseGraph() {
                 color = MaterialTheme.colorScheme.primary
             )
 
+            var xStep by remember { mutableStateOf(1) }
+
             LineChart(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(200.dp),
-                data = data
+                data = data,
+                xStep = xStep
             )
 
-            OutlinedButton(
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                onClick = { /*TODO*/ },
-                shape = MaterialTheme.shapes.small,
-                border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary.copy(0.5f))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
             ) {
-                Text(text = "Zoom", color = MaterialTheme.colorScheme.primary.copy(0.5f))
+
+                IconButton(
+                    onClick = { xStep -= 1 },
+                    enabled = xStep > 1
+                ) {
+                    Icon(imageVector = Icons.Outlined.Remove, contentDescription = "Remove")
+                }
+
+                OutlinedButton(
+                    onClick = {
+                        if (xStep < 3) xStep += 1
+                    },
+                    shape = MaterialTheme.shapes.small,
+                    enabled = false,
+                    border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary.copy(0.5f))
+                ) {
+                    Text(text = "Zoom", color = MaterialTheme.colorScheme.primary.copy(0.5f))
+                }
+
+                IconButton(
+                    onClick = { xStep += xStep },
+                    enabled = xStep < 3
+                ) {
+                    Icon(imageVector = Icons.Outlined.Add, contentDescription = "Add")
+                }
+
             }
         }
     }
@@ -201,9 +234,9 @@ fun LineChart(
     ) {
 
         // X cord label
-        (data.indices).forEach { i ->
+        (data.indices step xStep).forEach { i ->
             val hour = data[i].first
-            val x = graphStartPosition + i * xSpace
+            val x = (graphStartPosition + i * xSpace) / xStep
             drawContext.canvas.nativeCanvas.apply {
                 drawText(
                     hour.toString(),
@@ -235,7 +268,7 @@ fun LineChart(
                 val info = data[i]
                 val ratio = (info.second - lowerValue) / (upperValue - lowerValue)
 
-                val x1 = i * xSpace + graphStartPosition
+                val x1 = (i * xSpace + graphStartPosition) / xStep
                 val y1 = abs(height - ySpace - (ratio * height).toFloat())
 
                 if (i == 0) {
@@ -261,5 +294,5 @@ fun LineChart(
 @Preview
 @Composable
 fun PreviewDayExpenseGraph() {
-    DayExpenseGraph()
+    DayExpenseGraph(data)
 }
--- app/src/main/java/com/hcapps/xpenzave/presentation/general_stats/GeneralSection.kt
+++ app/src/main/java/com/hcapps/xpenzave/presentation/general_stats/GeneralSection.kt
@@ -2,13 +2,18 @@ package com.hcapps.xpenzave.presentation.general_stats
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.hcapps.xpenzave.presentation.compare.result.component.expense_category_graph.CategoryData
+import com.hcapps.xpenzave.presentation.compare.result.component.expense_category_graph.CategoryExpensesGraph
+import com.hcapps.xpenzave.presentation.home.component.BudgetProgressCard
 
 @Composable
 fun GeneralSection(modifier: Modifier = Modifier) {
@@ -21,7 +26,7 @@ fun GeneralSection(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.spacedBy(22.dp)
     ) {
 
-        /*BudgetProgressCard(
+        BudgetProgressCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight(0.45f),
@@ -31,9 +36,9 @@ fun GeneralSection(modifier: Modifier = Modifier) {
 
         CategoryExpensesGraph(
             categories = CategoryData.defaultCategoryGraphs()
-        )*/
+        )
 
-        DayExpenseGraph()
+        DayExpenseGraph(data)
 
     }
 
